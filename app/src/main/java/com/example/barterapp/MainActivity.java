770e3c388ab195e7b575ecf3c6527642242072d0
casey-tai <ceasedinsanity.ct@gmail.com>
2022-10-12 17:08:54 -0300

    added firebase to project and made account on registration

--- app/src/main/java/com/example/barterapp/MainActivity.java
+++ app/src/main/java/com/example/barterapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.barterapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -17,13 +18,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText firstName;
     EditText lastName;
     EditText address;
     EditText email;
+    EditText password;
     Button register;
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         firstName = findViewById(R.id.firstName);
         lastName = findViewById(R.id.lastName);
         email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
         register = findViewById(R.id.register);
 
         register.setOnClickListener(new View.OnClickListener() {
@@ -41,11 +52,40 @@ public class MainActivity extends AppCompatActivity {
                 checkDataEntered();
             }
         });
+
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+        }
+    }
+
+    private void registerUser(String email, String password) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Registered", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     boolean isEmail(EditText text) {
         CharSequence email = text.getText().toString();
         return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+    }
+
+    boolean isPassword(EditText text) {
+        CharSequence password = text.getText().toString();
+        return !TextUtils.isEmpty(password);
     }
 
     boolean isEmpty(EditText text) {
@@ -54,18 +94,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void checkDataEntered() {
+        boolean isError = false;
+
         if (isEmpty(firstName)) {
             Toast t = Toast.makeText(this, "You must enter first name to register!", Toast.LENGTH_SHORT);
             t.show();
+            isError = true;
         }
 
         if (isEmpty(lastName)) {
             lastName.setError("Last name is required");
+            isError = true;
         }
 
         if (isEmail(email) == false) {
             email.setError("Enter valid email");
+            isError = true;
         }
 
+        if (isPassword(password) == false) {
+            password.setError("Password is required");
+            isError = true;
+        }
+
+        if (!isError) {
+            registerUser("hello@hi.com", "password");
+        }
     }
 }
