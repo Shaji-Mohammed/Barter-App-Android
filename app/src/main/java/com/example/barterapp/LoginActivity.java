d3811fe9a708ce4a92904179be1f6f2e20881554
Carmahn McCalla <cr357456@dal.ca>
2022-10-12 20:08:16 -0300

    pulll changes before merge

--- app/src/main/java/com/example/barterapp/LoginActivity.java
+++ app/src/main/java/com/example/barterapp/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.barterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,6 +11,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button b1 = (Button) findViewById(R.id)
+
+
     }
 
 
