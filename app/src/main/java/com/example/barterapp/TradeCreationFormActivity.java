45bebf3d19fb048a42fba4b63a7150b16a287ee3
Chris <ceasedinsanity.ct@gmail.com>
2022-11-06 20:17:26 -0400

    Created Trade Creation Form

---
+++ app/src/main/java/com/example/barterapp/TradeCreationFormActivity.java
@@ -0,0 +1,14 @@
+package com.example.barterapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TradeCreationFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trade_creation_form);
+    }
+}
