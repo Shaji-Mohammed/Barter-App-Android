6c1dcec63c14932640cfb042ab1dc305be3f0d00
Chris <ceasedinsanity.ct@gmail.com>
2022-12-07 02:21:02 -0400

    Added date and price to trades

--- app/src/main/java/com/example/barterapp/Trade.java
+++ app/src/main/java/com/example/barterapp/Trade.java
@@ -1,9 +1,13 @@
 package com.example.barterapp;
 
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,10 +18,13 @@ public class Trade {
         this.firestore = firestore;
     }
 
-    public Task<DocumentReference> save(String name, String desc) {
-        Map<String, String> data = new HashMap<>();
+    public Task<DocumentReference> save(String userId, String name, String desc, Double estimatedPrice) {
+        Map<String, Object> data = new HashMap<>();
+        data.put("owner", firestore.document("users/" + userId));
         data.put("name", name);
         data.put("desc", desc);
+        data.put("estimatedPrice", estimatedPrice);
+        data.put("posted", new Timestamp(new Date()));
         return this.firestore.collection("trades").add(data);
     }
 }
--- app/src/main/java/com/example/barterapp/TradeCreationFormActivity.java
+++ app/src/main/java/com/example/barterapp/TradeCreationFormActivity.java
@@ -1,6 +1,5 @@
 package com.example.barterapp;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,23 +7,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.NumberPicker;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class TradeCreationFormActivity extends AppCompatActivity {
 
-    FirebaseFirestore firestore;
+    private FirebaseUser user;
 
-    EditText itemEditText;
-    EditText descEditText;
-    Button backButton;
+    private EditText itemEditText;
+    private EditText descEditText;
+    private EditText estimatedPriceText;
 
     private Trade trade;
 
@@ -33,22 +29,18 @@ public class TradeCreationFormActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trade_creation_form);
 
-        itemEditText = findViewById(R.id.createItemFormItemName);
-        descEditText = findViewById(R.id.createItemFormDesc);
+        itemEditText = findViewById(R.id.trade_create_name);
+        descEditText = findViewById(R.id.trade_create_desc);
+        estimatedPriceText = findViewById(R.id.trade_create_estimate);
 
         Button createItemButton = findViewById(R.id.createItemSubmitButton);
-        createItemButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) { createItem(); }
-        });
+        createItemButton.setOnClickListener(view -> createItem());
 
         Button backButton = findViewById(R.id.createTradeFormBack);
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) { switchToItemFeedWindow(); }
-        });
+        backButton.setOnClickListener(view -> switchToItemFeedWindow());
 
-        firestore = FirebaseFirestore.getInstance();
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
 
         trade = new Trade(firestore);
     }
@@ -63,6 +55,12 @@ public class TradeCreationFormActivity extends AppCompatActivity {
 
         String name = itemEditText.getText().toString();
         String desc = descEditText.getText().toString();
+        Double estimatedPrice = 0.0;
+        try {
+            estimatedPrice = Double.parseDouble(estimatedPriceText.getText().toString());
+        } catch (NumberFormatException e) {
+            isError = true;
+        }
 
         if (!FormValidator.isValidItemName(name)) {
             itemEditText.setError("Item name must be " + FormValidator.MIN_ITEM_NAME_LENGTH + "-" + FormValidator.MAX_ITEM_NAME_LENGTH + " characters.");
@@ -75,19 +73,12 @@ public class TradeCreationFormActivity extends AppCompatActivity {
         }
 
         if (!isError) {
-            trade.save(name, desc)
-                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                        @Override
-                        public void onSuccess(DocumentReference documentReference) {
-                            Toast.makeText(TradeCreationFormActivity.this, "Data added", Toast.LENGTH_SHORT).show();
-                        }
+            trade.save(user.getUid(), name, desc, estimatedPrice)
+                    .addOnSuccessListener(documentReference -> {
+                        Toast.makeText(TradeCreationFormActivity.this, "Posted", Toast.LENGTH_SHORT).show();
+                        switchToItemFeedWindow();
                     })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Toast.makeText(TradeCreationFormActivity.this, "Failed to add: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    });
+                    .addOnFailureListener(e -> Toast.makeText(TradeCreationFormActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show());
         }
 
 
